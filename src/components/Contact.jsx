c979e79246378a928fb82fa4723076e3a276feb7
Skmdtamizhan <sktamilan878@gmail.com>
2026-05-09 20:22:13 +0530

    updated backend api url

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     setErrorMsg('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://sunilkumarrks-portfolio.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
